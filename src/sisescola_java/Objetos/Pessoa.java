c630e9e86d1e76389c3a6381406841665531a78e
Gabriel Casanova <48224013+gbcasanova@users.noreply.github.com>
2024-02-27 16:13:43 -0300

    Primeiro commit

---
+++ src/sisescola_java/Objetos/Pessoa.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sisescola_java.Objetos;
+
+
+public class Pessoa {
+    private int CPF;
+    private String Nome;
+    private String Endereco;
+}
